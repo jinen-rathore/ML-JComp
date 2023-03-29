3d314b4ae0df50a25efcf90e612261b873921ca2
Jinen Rathore <jinenrathore@gmail.com>
2023-03-29 18:04:54 +0530

    Delete plag.py

--- plag.py
+++
@@ -1,6 +0,0 @@
-from copydetect import CopyDetector
-
-detector = CopyDetector(test_dirs = ['docs'], extensions = ['py'], display_t = 0.5)
-detector.add_file("copydetect/utils.py")
-detector.run()
-detector.generate_html_report()
